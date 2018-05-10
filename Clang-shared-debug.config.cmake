e40d58c70719fe169a5504f05dd7d1b602b61e6c
Knitschi <knitschi@freenet.de>
2018-05-10 15:14:02 +0200

    Fix test files directories

--- Clang-shared-debug.config.cmake
+++ Clang-shared-debug.config.cmake
@@ -9,5 +9,6 @@ set( BUILD_SHARED_LIBS ON CACHE BOOL "Set this to ON to create all production ta
 set( CPF_ENABLE_ABI_API_COMPATIBILITY_CHECK_TARGETS ON CACHE BOOL "Enables targets that create ABI/API compatibility reports and checking." FORCE)
 
 # Locations
+set( CPF_TEST_FILES_DIR "C:/Temp/CPF_tests/${CPF_CONFIG}" CACHE PATH "The directory under which the automated tests may create temporary files." FORCE )
 set( CPF_WEBPAGE_URL "http://buildmasterdebian9:8082" CACHE STRING "An url from which the distribution of the last build can be downloaded." FORCE)
 set( Qt5_DIR "/usr/local/Qt-5.9.1/debug/lib/cmake/Qt5" CACHE STRING "Location of Qt" FORCE)
