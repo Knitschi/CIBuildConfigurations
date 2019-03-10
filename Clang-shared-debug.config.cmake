3cca716e013503d1050762396d2b7b1afacb81b6
Knitschi <11889116+Knitschi@users.noreply.github.com>
2019-03-10 16:00:47 +0100

    Updates clang-tidy version

--- Clang-shared-debug.config.cmake
+++ Clang-shared-debug.config.cmake
@@ -8,7 +8,9 @@ set( BUILD_SHARED_LIBS ON CACHE BOOL "Set this to ON to create all production ta
 
 set( CPF_ENABLE_ABI_API_COMPATIBILITY_REPORT_TARGETS ON CACHE BOOL "Enables targets that create ABI/API compatibility reports and checking." FORCE)
 set( CPF_ENABLE_CLANG_FORMAT_TARGETS ON CACHE BOOL "Activates custom targets that run clang-format." FORCE)
-set( CPF_CLANG_FORMAT_EXE "clang-format-6.0" CACHE STRING "name of the clang-format tool" FORCE)
+set( CPF_CLANG_FORMAT_EXE "clang-format-6.0" CACHE STRING "name of the clang-format executable" FORCE)
+set( CPF_CLANG_TIDY_EXE "clang-tidy-4.0" CACHE STRING "name of the clang-tidy executable" FORCE)
+
 
 # Locations
 set( CPF_TEST_FILES_DIR "~/Temp/CPF_tests/${CPF_CONFIG}" CACHE PATH "The directory under which the automated tests may create temporary files." FORCE )
