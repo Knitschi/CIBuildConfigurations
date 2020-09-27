308ad2ba8ac7c2d9ae90806213be997cff6fd37e
Knitschi <knitschi@freenet.de>
2020-09-27 17:40:27 +0200

    Fixes a bug where VS2019-static-release did not build static.

--- VS2019-static-release.config.cmake
+++ VS2019-static-release.config.cmake
@@ -4,5 +4,5 @@ set(CPF_CONFIG "VS2019-static-release" CACHE STRING "the config name" FORCE)
 include( "${CMAKE_CURRENT_LIST_DIR}/VS2019-shared.config.cmake" )
 
 # GENERATOR AND TOOLCHAIN
-set( BUILD_SHARED_LIBS ON CACHE BOOL "Set this to ON to create all production target libraries as shared libries. The fixture libraries and libraries created for executables are always static libraries." FORCE)
+set( BUILD_SHARED_LIBS OFF CACHE BOOL "Set this to ON to create all production target libraries as shared libries. The fixture libraries and libraries created for executables are always static libraries." FORCE)
 set( CMAKE_CONFIGURATION_TYPES Release CACHE STRING "" FORCE)
